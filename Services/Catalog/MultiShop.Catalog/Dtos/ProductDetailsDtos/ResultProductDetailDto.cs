bebdc4baba33aaf7bed18b9ee59a68bb169fdd06
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-04 09:58:52 +0300

    productdetaildto

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductDetailsDtos/ResultProductDetailDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.Catalog.Dtos.ProductDetailsDtos
+{
+    public class ResultProductDetailDto
+    {
+        public string ProductDetailID { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductInfo { get; set; }
+
+        public string ProductId { get; set; }
+
+    }
+}
