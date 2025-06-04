0bb9b1068f216d4ea89050152e723072c84440c2
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-04 10:26:40 +0300

    category service ctor

---
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryServices/CategoryService.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.CategoryServices
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IMongoCollection<Category> _categoryCollection;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IMapper mapper,IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString);  //bağlantı
+            var database = client.GetDatabase(_databaseSettings.DatabaseName); //database 
+            _categoryCollection = database.GetCollection<Category>(_databaseSettings.CategoryCollectionName); //tablolar
+            _mapper = mapper;                                                   
+        }
+
+        public Task CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCategoryAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
