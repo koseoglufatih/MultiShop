933267936167b6d736b641e1a38482bd9e1526c8
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-04 10:06:20 +0300

    mapping

---
+++ Services/Catalog/MultiShop.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Dtos.ProductDtos;
+using MultiShop.Catalog.Entities;
+
+namespace MultiShop.Catalog.Mapping
+{
+    public class GeneralMapping :Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Category,ResultCategoryDto>().ReverseMap();
+            CreateMap<Category,CreateCategoryDto>().ReverseMap();
+            CreateMap<Category,UpdateCategoryDto>().ReverseMap();
+            CreateMap<Category, GetByIdCategoryDto>().ReverseMap();
+
+            CreateMap<Product, ResultProductDto>().ReverseMap();
+            CreateMap<Product, CreateProductDto>().ReverseMap();
+            CreateMap<Product, UpdateProductDto>().ReverseMap();
+            CreateMap<Product, GetByIdProductDto>().ReverseMap();
+
+
+        }
+
+    }
+}
