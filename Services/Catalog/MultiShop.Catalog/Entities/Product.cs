f583795bfec64f43d8abf04eae406eab7ea3dc84
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-04 09:46:52 +0300

    categorydto

--- Services/Catalog/MultiShop.Catalog/Entities/Product.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/Product.cs
@@ -1,5 +1,5 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
-using MongoDB.Bson;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace MultiShop.Catalog.Entities
 {
@@ -13,7 +13,7 @@ namespace MultiShop.Catalog.Entities
         public string ProductImageUrl { get; set; }
         public string ProductDescription { get; set; }
 
-    
+
         public string CategoryID { get; set; }
 
         [BsonIgnore]
