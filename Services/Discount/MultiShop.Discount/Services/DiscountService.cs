cf35eb117e7a6ff2f60bcdaa454395a170512c0a
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-10 12:49:27 +0300

    dapperadd

--- Services/Discount/MultiShop.Discount/Services/DiscountService.cs
+++ Services/Discount/MultiShop.Discount/Services/DiscountService.cs
@@ -1,6 +1,47 @@
-﻿namespace MultiShop.Discount.Services
+﻿using Dapper;
+using MultiShop.Discount.Context;
+using MultiShop.Discount.Dtos;
+
+namespace MultiShop.Discount.Services
 {
-    public class DiscountService
+    public class DiscountService : IDiscountService
     {
+        private readonly DapperContext _dapperContext;
+
+        public DiscountService(DapperContext dapperContext)
+        {
+            _dapperContext = dapperContext;
+        }
+
+        public Task CreateCouponAsync(CreateCouponDto createCouponDto)
+        {
+            string query = "insert into Coupons(Code,Rate,IsActive,ValidDate) values (@code,@rate,@isActive,@validDate)";
+            var parameters = new DynamicParameters();
+            parameters.Add("@code", createCouponDto.Code);
+            parameters.Add("@rate", createCouponDto.Rate);
+            parameters.Add("@isActive", createCouponDto.IsActive);
+            parameters.Add("@validDate", createCouponDto.ValidDate);
+
+        }
+
+        public Task DeleteCouponAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCouponDto>> GetAllCouponsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIdCouponDto> GetByIdCouponAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCouponAsync(UpdateCouponDto updateCouponDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
