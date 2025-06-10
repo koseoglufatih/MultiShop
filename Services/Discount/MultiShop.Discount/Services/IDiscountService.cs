432ef9171a5d45b68c68c6856cff03705540af98
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-10 12:41:37 +0300

    dto and services

--- Services/Discount/MultiShop.Discount/Services/IDiscountService.cs
+++ Services/Discount/MultiShop.Discount/Services/IDiscountService.cs
@@ -5,5 +5,9 @@ namespace MultiShop.Discount.Services
     public interface IDiscountService
     {
         Task<List<ResultCouponDto>> GetAllCouponsAsync();
+        Task CreateCouponAsync(CreateCouponDto createCouponDto);
+        Task UpdateCouponAsync(UpdateCouponDto updateCouponDto);
+        Task DeleteCouponAsync(int id);
+        Task<GetByIdCouponDto> GetByIdCouponAsync(int id);  
     }
 }
