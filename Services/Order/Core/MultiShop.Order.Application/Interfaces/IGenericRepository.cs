479551edbd50804b681b953c0c6e256ef408bed5
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-10 23:33:25 +0300

    cqrs

---
+++ Services/Order/Core/MultiShop.Order.Application/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> CreateAsync(T entity);  
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+        Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter);
+
+    }
+}
