807dc85b7b96616d14634d0b82b3e64fc086464c
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-11 11:58:07 +0300

    getorderingbyidqueryhandler

---
+++ Services/Order/Applications/MultiShop.Order.Applications/Features/Mediator/Handlers/OrderingHandlers/GetOrderingByIdQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using MultiShop.Order.Applications.Features.CQRS.Results.OrderDetailResults;
+using MultiShop.Order.Applications.Features.Mediator.Queries.OrderingQueries;
+using MultiShop.Order.Applications.Features.Mediator.Results.OrderingResults;
+using MultiShop.Order.Applications.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Applications.Features.Mediator.Handlers.OrderingHandlers
+{
+    public class GetOrderingByIdQueryHandler : IRequestHandler<GetOrderingByIdQuery, GetOrderingByIdQueryResult>
+    {
+        private readonly IGenericRepository<Ordering> _genericRepository;
+
+        public GetOrderingByIdQueryHandler(IGenericRepository<Ordering> genericRepository)
+        {
+            _genericRepository = genericRepository;
+        }
+
+        public async Task<GetOrderingByIdQueryResult> Handle(GetOrderingByIdQuery request, CancellationToken cancellationToken)
+        {
+          var values = await _genericRepository.GetByIdAsync(request.Id);
+            return new GetOrderingByIdQueryResult
+            {
+                OrderDate = values.OrderDate,
+                OrderingId = values.OrderingId,
+                TotalPrice = values.TotalPrice,
+                UserId = values.UserId
+            };
+
+        }
+    }
+}
+
