55b7661aa27b7cf3a0ff6786ceb11249c39f7744
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-11 08:41:54 +0300

    createaddresscommand

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/CreateAddressCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MultiShop.Order.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class CreateAddressCommandHandler
+    {
+        private readonly IGenericRepository<Address> _repository;
+
+        public CreateAddressCommandHandler(IGenericRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateAddressCommand createAddressCommand)
+        {
+            await _repository.CreateAsync(new Address
+            {
+                City = createAddressCommand.City,
+                Detail = createAddressCommand.Detail,
+                District = createAddressCommand.District,
+                UserId = createAddressCommand.UserId
+            });
+        }
+    }
+}
