04efab8ffc0ce50ed70c09c2a9b185e37df17f64
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-11 11:20:39 +0300

    mediatr classor

---
+++ Services/Order/Applications/MultiShop.Order.Applications/Features/Mediator/Queries/OrderingQueries/GetOrderingByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using MultiShop.Order.Applications.Features.Mediator.Results.OrderingResults;
+
+namespace MultiShop.Order.Applications.Features.Mediator.Queries.OrderingQueries
+{
+    public class GetOrderingByIdQuery : IRequest<GetOrderingByIdQueryResult>
+    {
+        public int Id { get; set; }
+
+        public GetOrderingByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ Services/Order/Applications/MultiShop.Order.Applications/Features/Mediator/Queries/OrderingQueries/GetOrderingQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using MultiShop.Order.Applications.Features.Mediator.Results.OrderingResults;
+
+namespace MultiShop.Order.Applications.Features.Mediator.Queries.OrderingQueries
+{
+    public class GetOrderingQuery : IRequest<List<GetOrderingQueryResult>>
+    {
+
+    }
+}
