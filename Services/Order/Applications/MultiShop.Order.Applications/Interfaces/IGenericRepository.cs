5471ac4c4eb1721c34fe55dfe9ef9ce60c2bc8be
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-11 09:41:52 +0300

    repositories

--- Services/Order/Applications/MultiShop.Order.Applications/Interfaces/IGenericRepository.cs
+++ Services/Order/Applications/MultiShop.Order.Applications/Interfaces/IGenericRepository.cs
@@ -7,9 +7,9 @@ namespace MultiShop.Order.Applications.Interfaces
     {
         Task<List<T>> GetAllAsync();
         Task<T> GetByIdAsync(int id);
-        Task<T> CreateAsync(T entity);
-        Task<T> UpdateAsync(T entity);
-        Task<T> DeleteAsync(T entity);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
         Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter);
 
     }
