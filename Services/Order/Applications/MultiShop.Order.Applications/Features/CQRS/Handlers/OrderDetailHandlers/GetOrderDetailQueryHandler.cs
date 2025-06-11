487b1e712182a00211e1245edc736e6ccebb5f95
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-11 10:02:58 +0300

    orderdetailscrqs

---
+++ Services/Order/Applications/MultiShop.Order.Applications/Features/CQRS/Handlers/OrderDetailHandlers/GetOrderDetailQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using MultiShop.Order.Applications.Features.CQRS.Results.OrderDetailResults;
+using MultiShop.Order.Applications.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Applications.Features.CQRS.Handlers.OrderDetailHandlers
+{
+    public class GetOrderDetailQueryHandler
+    {
+        private readonly IGenericRepository<OrderDetail> _repository;
+
+        public GetOrderDetailQueryHandler(IGenericRepository<OrderDetail> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetOrderDetailByIdQueryResult>> Handle()
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(x => new GetOrderDetailByIdQueryResult
+            {
+                OrderDetailId = x.OrderDetailId,
+                ProductAmount   = x.ProductAmount,
+                OrderingId = x.OrderingId,
+                ProductId = x.ProductId,
+                ProductName = x.ProductName,
+                ProductPrice = x.ProductPrice,
+                ProductTotalPrice = x.ProductTotalPrice 
+            }).ToList();
+        }
+    }
+}
