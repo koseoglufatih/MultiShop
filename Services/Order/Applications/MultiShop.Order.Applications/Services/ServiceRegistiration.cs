8102c52e34ef1ffca6073b54d1e7b8de974bb658
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-11 13:30:43 +0300

    serviceregistration

---
+++ Services/Order/Applications/MultiShop.Order.Applications/Services/ServiceRegistiration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MultiShop.Order.Applications.Services
+{
+    public static class ServiceRegistiration
+    {
+        public static void AddApplicationService(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddMediatR(cfg=>cfg.RegisterServicesFromAssembly(typeof(ServiceRegistiration).Assembly));
+
+
+        }
+    }
+}
