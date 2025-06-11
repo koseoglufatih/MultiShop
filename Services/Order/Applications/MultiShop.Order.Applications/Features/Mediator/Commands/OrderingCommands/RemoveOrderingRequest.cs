708e715916f1afeabd9e0caec16516a0b7359260
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-11 11:43:53 +0300

    orderingommands class mediator

---
+++ Services/Order/Applications/MultiShop.Order.Applications/Features/Mediator/Commands/OrderingCommands/RemoveOrderingRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace MultiShop.Order.Applications.Features.Mediator.Commands.OrderingCommands
+{
+    public class RemoveOrderingRequest :IRequest
+    {
+        public int Id { get; set; }
+
+        public RemoveOrderingRequest(int id)
+        {
+            Id = id;
+        }
+    }
+}
