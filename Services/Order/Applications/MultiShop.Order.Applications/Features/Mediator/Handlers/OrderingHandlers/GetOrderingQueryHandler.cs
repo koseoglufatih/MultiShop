e796e941c51d6cdfa3fcc2e9872b7a036fec3387
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-11 11:49:03 +0300

    getorderhandling

---
+++ Services/Order/Applications/MultiShop.Order.Applications/Features/Mediator/Handlers/OrderingHandlers/GetOrderingQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using MultiShop.Order.Applications.Features.Mediator.Queries.OrderingQueries;
+using MultiShop.Order.Applications.Features.Mediator.Results.OrderingResults;
+using MultiShop.Order.Applications.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Applications.Features.Mediator.Handlers.OrderingHandlers
+{
+    public class GetOrderingQueryHandler : IRequestHandler<GetOrderingQuery, List<GetOrderingQueryResult>>
+    {
+        private readonly IGenericRepository<Ordering> _genericRepository;
+
+        public GetOrderingQueryHandler(IGenericRepository<Ordering> genericRepository)
+        {
+            _genericRepository = genericRepository;
+        }
+
+        public async Task<List<GetOrderingQueryResult>> Handle(GetOrderingQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _genericRepository.GetAllAsync();
+            return values.Select(x=>new GetOrderingQueryResult
+            {
+                OrderingId  = x.OrderingId,
+                OrderDate = x.OrderDate,
+                TotalPrice = x.TotalPrice,
+                UserId = x.UserId,
+            }).ToList();
+        }
+    }
+}
