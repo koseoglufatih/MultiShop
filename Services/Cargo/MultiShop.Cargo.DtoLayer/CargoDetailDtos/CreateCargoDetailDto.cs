3d8f531e8bd3124b49ff9780653bf8acb229bdc8
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-12 17:41:16 +0300

    cargodetailcontrollers

---
+++ Services/Cargo/MultiShop.Cargo.DtoLayer/CargoDetailDtos/CreateCargoDetailDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiShop.Cargo.DtoLayer.CargoDetailDtos
+{
+    public class CreateCargoDetailDto
+    {
+        public string SenderCustomer { get; set; }
+        public string ReceiverCustomer { get; set; }
+        public int Barcode { get; set; }
+        public int CargoCompanyId { get; set; }
+        public CargoCompany CargoCompany { get; set; }
+    }
+}
