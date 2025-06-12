2936965ea57331c92922d80e1820d804103a90c2
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-12 18:06:55 +0300

    registration

--- Services/Cargo/MultiShop.Cargo.DtoLayer/CargoDetailDtos/CreateCargoDetailDto.cs
+++ Services/Cargo/MultiShop.Cargo.DtoLayer/CargoDetailDtos/CreateCargoDetailDto.cs
@@ -6,6 +6,5 @@
         public string ReceiverCustomer { get; set; }
         public int Barcode { get; set; }
         public int CargoCompanyId { get; set; }
-        public CargoCompany CargoCompany { get; set; }
     }
 }
