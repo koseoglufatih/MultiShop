71bb5a3d20b517cd421ba7187467f5c3f5655743
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-12 15:21:11 +0300

    cargo

--- Services/Cargo/MultiShop.Cargo.EntityLayer/Concrete/CargoDetail.cs
+++ Services/Cargo/MultiShop.Cargo.EntityLayer/Concrete/CargoDetail.cs
@@ -9,8 +9,11 @@ namespace MultiShop.Cargo.EntityLayer.Concrete
     public class CargoDetail
     {
         public int CargoDetailId { get; set; }
-        public int CargoCustomerId { get; set; }
-        public CargoCustomer CargoCustomer { get; set; }
-        public string  ReceiverCustomer { get; set; }
+        public string SenderCustomer { get; set; }
+        public string ReceiverCustomer { get; set; }
+        public int Barcode { get; set; }
+        public int CargoCompanyId { get; set; }
+        public CargoCompany CargoCompany { get; set; }
+
     }
 }
