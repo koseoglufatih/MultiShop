fa7bf552e56ef13f74a815d2f0c35d03958d2651
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-16 14:41:33 +0300

    öne çıkan ürünler

---
+++ WebUI/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultCompentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Buffers;
+
+namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _FeatureProductsDefaultCompentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+
+        {
+            return View();
+        }
+    }
+}
