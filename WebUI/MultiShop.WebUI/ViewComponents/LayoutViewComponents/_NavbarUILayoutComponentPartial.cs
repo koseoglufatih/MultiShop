46a2e8bbda2f8721de98616de1a475fdb498ae05
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-16 12:55:34 +0300

    navbaruiviewcomponents

---
+++ WebUI/MultiShop.WebUI/ViewComponents/LayoutViewComponents/_NavbarUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.LayoutViewComponents
+{
+    public class _NavbarUILayoutComponentPartial :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
