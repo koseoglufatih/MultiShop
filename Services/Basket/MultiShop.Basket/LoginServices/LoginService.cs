d1d1be8dfef56447a358e25feb39ae106f0ba36f
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-16 09:12:51 +0300

    httpcobtext sub

---
+++ Services/Basket/MultiShop.Basket/LoginServices/LoginService.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiShop.Basket.LoginServices
+{
+    public class LoginService : ILoginService
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public LoginService(IHttpContextAccessor contextAccessor)
+        {
+            httpContextAccessor = contextAccessor;
+        }
+
+        public string GetUserId => httpContextAccessor.HttpContext.User.FindFirst("sub").Value;
+}
