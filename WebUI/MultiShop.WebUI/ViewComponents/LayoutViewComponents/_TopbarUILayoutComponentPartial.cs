aa2aa2a0ea080bdb7b65ca23d18e662144c3cca1
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-16 12:37:17 +0300

    topbaruilayout

---
+++ WebUI/MultiShop.WebUI/ViewComponents/LayoutViewComponents/_TopbarUILayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.LayoutViewComponents
+{
+    public class _TopbarUILayoutComponentPartial :ViewComponent
+     {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
