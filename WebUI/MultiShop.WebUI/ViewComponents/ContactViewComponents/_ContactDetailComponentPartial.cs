fa5c308e26d97e8e8e0e26efc8b5236460a97b98
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-17 11:16:57 +0300

    contactus

---
+++ WebUI/MultiShop.WebUI/ViewComponents/ContactViewComponents/_ContactDetailComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace MultiShop.WebUI.ViewComponents.ContactViewComponents
+{
+    public class _ContactDetailComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        
+        }
+    }
+}
