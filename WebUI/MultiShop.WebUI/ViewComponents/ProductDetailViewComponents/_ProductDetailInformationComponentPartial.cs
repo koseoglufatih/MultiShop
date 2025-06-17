87a46a24a082a6ab88485e0bf21a542732cabcdd
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-17 10:29:22 +0300

    product review component partial

---
+++ WebUI/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailInformationComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NuGet.Versioning;
+
+namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponents
+{
+    public class _ProductDetailInformationComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
