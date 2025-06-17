89c84f2c90ddd66448d355c9fedf752278b86e3f
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-17 12:39:29 +0300

    categorysayfasi

--- WebUI/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ WebUI/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -2,10 +2,15 @@
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         public IActionResult Index()
         {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Kategoriler";
+            ViewBag.v3 = "Kategori Listesi";
+            ViewBag.v0 = "Kategori İşlemleri";
             return View();
         }
     }
