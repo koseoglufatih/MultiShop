3b682d465d3341b8271c969b9ca701eecda83b51
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-19 10:31:42 +0300

    specialoffer

--- WebUI/MultiShop.WebUI/Areas/Admin/Controllers/FeatureSliderController.cs
+++ WebUI/MultiShop.WebUI/Areas/Admin/Controllers/FeatureSliderController.cs
@@ -6,6 +6,7 @@ using System.Text;
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Route("Admin/FeatureSlider")]
     public class FeatureSliderController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
@@ -23,14 +24,18 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
             ViewBag.v0 = "Öne Çıkan Slider Görsel İşlemleri";
 
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7236/api/FeatureSlider");
-            if (responseMessage.IsSuccessStatusCode)
+            var response = await client.GetAsync("https://localhost:7236/api/FeatureSliders"); 
+
+            if (!response.IsSuccessStatusCode)
             {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultFeatureSliderDto>>(jsonData);
-                return View(values);
+               
+                return View(new List<ResultFeatureSliderDto>());
             }
-            return View();
+
+            var jsonData = await response.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultFeatureSliderDto>>(jsonData);
+
+            return View(values);
         }
 
 
@@ -59,7 +64,7 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
             {
                 return RedirectToAction("Index", "FeatureSlider", new { area = "Admin" });
             }
-            return View();
+            return View(new CreateFeatureSliderDto());
         }
 
         [Route("DeleteFeatureSlider/{id}")]
