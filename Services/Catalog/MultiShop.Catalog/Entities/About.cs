171f5e8fb6ab198d5e25dee700491f1c48422c6c
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-24 09:54:47 +0300

    about

---
+++ Services/Catalog/MultiShop.Catalog/Entities/About.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MultiShop.Catalog.Entities
+{
+    public class About
+    {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string AboutId { get; set; }
+        public string Description { get; set; }
+        public string Adress { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
