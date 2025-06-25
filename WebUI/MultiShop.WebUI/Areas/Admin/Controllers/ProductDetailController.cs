a5c139fd2998aff5b16e7452390114b252c3c5f1
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-25 09:26:11 +0300

    productdetailpage

---
+++ WebUI/MultiShop.WebUI/Areas/Admin/Controllers/ProductDetailController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ProductDetailDto;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    
+    [Area("Admin")]
+    [AllowAnonymous]
+    [Route("Admin/ProductDetail")]
+    public class ProductDetailController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductDetailController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [Route("o/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateProductDetail(string id)
+        {
+
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Ürünler";
+            ViewBag.v3 = "Ürün Açıklama ve Bilgi Güncelleme Sayfası";
+            ViewBag.v0 = "Ürün İşlemleri";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7236/api/ProductDetails/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateProductDetailDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [Route("o/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> o(UpdateProductDetailDto updateProductDetailDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var JsonData = JsonConvert.SerializeObject(updateProductDetailDto);
+            StringContent stringContent = new StringContent(JsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync($"https://localhost:7236/api/ProductDetails/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ProductListWithCategory", "Product", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
