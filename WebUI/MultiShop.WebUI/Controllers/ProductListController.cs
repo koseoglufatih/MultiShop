b65926d850a8262bef164233f06279ac6bdf51f0
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-25 14:54:52 +0300

    /ProductList/AddComment

--- WebUI/MultiShop.WebUI/Controllers/ProductListController.cs
+++ WebUI/MultiShop.WebUI/Controllers/ProductListController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CommentDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class ProductListController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductListController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index(string id)
         {
             ViewBag.i = id;
@@ -13,6 +21,20 @@ namespace MultiShop.WebUI.Controllers
         public IActionResult ProductDetail(string id)
         {
             ViewBag.x = id;
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult AddComment()
+        {
+            return PartialView();
+        }
+
+
+        [HttpPost]
+        public IActionResult AddComment(CreateCommentDto createCommentDto)
+        {
+
             return View();
         }
     }
