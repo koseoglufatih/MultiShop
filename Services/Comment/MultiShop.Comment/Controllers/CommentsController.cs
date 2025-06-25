386152108e157eaea89bf3ab4dfa7e42fbf65fbc
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-25 11:37:10 +0300

    commentapi

---
+++ Services/Comment/MultiShop.Comment/Controllers/CommentsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Comment.Context;
+using MultiShop.Comment.Entities;
+
+namespace MultiShop.Comment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class CommentsController : ControllerBase
+    {
+        private readonly CommentContext _commentContext;
+
+        public CommentsController(CommentContext commentContext)
+        {
+            _commentContext = commentContext;
+        }
+
+        [HttpGet]
+        public IActionResult CommentList()
+        {
+            var values = _commentContext.UserComments.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateComment(UserComment userComment)
+        {
+            _commentContext.UserComments.Add(userComment);
+            _commentContext.SaveChanges();
+            return Ok("Yorum başarıyla eklendi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateComment(UserComment userComment)
+        {
+            _commentContext.UserComments.Update(userComment);
+            _commentContext.SaveChanges();
+            return Ok("Yorum başarıyla güncellendi");
+        }
+
+    }
+}
--- Services/Comment/MultiShop.Comment/Entities/UserComment.cs
+++ Services/Comment/MultiShop.Comment/Entities/UserComment.cs
@@ -10,5 +10,9 @@
         public int Rating { get; set; }
         public DateTime CreatedDate { get; set; }
         public bool Status { get; set; }
+
+        public int ProductId { get; set; }
+
     }
+
 }
