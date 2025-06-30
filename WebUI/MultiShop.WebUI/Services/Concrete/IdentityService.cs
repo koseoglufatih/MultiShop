ef1111ba82a1b0fad54bd396c9713f9388a0a862
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-30 12:11:46 +0300

    authentication token

--- WebUI/MultiShop.WebUI/Services/Concrete/IdentityService.cs
+++ WebUI/MultiShop.WebUI/Services/Concrete/IdentityService.cs
@@ -1,13 +1,46 @@
-﻿using MultiShop.DtoLayer.IdentityDtos.LoginDtos;
+﻿using IdentityModel.Client;
+using Microsoft.Extensions.Options;
+using MultiShop.DtoLayer.IdentityDtos.LoginDtos;
 using MultiShop.WebUI.Services.Interfaces;
+using MultiShop.WebUI.Settings;
+using NuGet.Packaging;
 
 namespace MultiShop.WebUI.Services.Concrete
 {
     public class IdentityService : IIdentityService
     {
-        public Task<bool> SignIn(SignUpDto signUpDto)
+        private readonly HttpClient _httpClient;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ClientSettings _clientSettings;
+
+        public IdentityService(HttpClient httpClient, IHttpContextAccessor httpContextAccessor, IOptions<ClientSettings> clientSettings)
         {
-            throw new NotImplementedException();
+            _httpClient = httpClient;
+            _httpContextAccessor = httpContextAccessor;
+            _clientSettings = clientSettings.Value;
+        }
+
+        public async Task<bool> SignIn(SignUpDto signUpDto)
+        {
+            var discoveryEndPoint = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+            {
+                Address = "https://localhost:44320",    //token ?
+                Policy = new DiscoveryPolicy
+                {
+                    RequireHttps = false,
+                }
+            });
+
+            var passwordTokenRequest = new PasswordTokenRequest
+            {
+                ClientId = _clientSettings.MultiShopManagerId.ClientId,
+                ClientSecret = _clientSettings.MultiShopManagerId.ClientSecret,
+                UserName = signUpDto.UserName,
+                Password = signUpDto.Password,
+                Address = discoveryEndPoint.TokenEndpoint
+            };
+
+
         }
     }
 }
--- WebUI/MultiShop.WebUI/Settings/ClientSettings.cs
+++ WebUI/MultiShop.WebUI/Settings/ClientSettings.cs
@@ -2,7 +2,9 @@
 {
     public class ClientSettings
     {
-
+        public Client MultiShopVisitorId { get; set; }
+        public Client MultiShopManagerId { get; set; }
+        public Client MultiShopAdminId { get; set; }
     }
     public class Client
     {
