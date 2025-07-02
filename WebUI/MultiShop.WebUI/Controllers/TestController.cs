7de1fb31f09f10ca207c3b1c64bc2c0037995a20
FatihKoseoglu <koseoglufatih@gmail.com>
2025-07-02 11:39:23 +0300

    token layout

--- WebUI/MultiShop.WebUI/Controllers/TestController.cs
+++ WebUI/MultiShop.WebUI/Controllers/TestController.cs
@@ -2,6 +2,7 @@
 using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
 
 namespace MultiShop.WebUI.Controllers
 {
@@ -17,7 +18,37 @@ namespace MultiShop.WebUI.Controllers
 
 		public async Task<IActionResult> Index()
 		{
-			var client = _httpClientFactory.CreateClient();
+
+            string token="";
+            using (var HttpClient = new HttpClient())
+            {
+                var request = new HttpRequestMessage
+                {
+                    RequestUri = new Uri("https://localhost:44320/api/Logins"),  //token ?
+                    Method = HttpMethod.Post,
+                    Content = new FormUrlEncodedContent(new Dictionary<string, string>
+                    {
+                        {"client_id","MultiShopVisitorId"},
+                        {"client_secret","multishopsecret"},
+                        {"grant_type","cleint_credentials"}
+                    })
+                };
+
+                using (var response = await HttpClient.SendAsync(request))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var tokenResponse = JObject.Parse(content);
+                        token = tokenResponse["access   _token"].ToString();
+                    }
+                }
+            }
+
+
+            var client = _httpClientFactory.CreateClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",token);
+
 			var responseMessage = await client.GetAsync("https://localhost:7236/api/Categories");
 			if (responseMessage.IsSuccessStatusCode)
 			{
--- WebUI/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
+++ WebUI/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MultiShop.DtoLayer.CatalogDtos.AboutDtos;
+using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
 
 namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
 {
@@ -16,15 +19,46 @@ namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
 
 		public async Task<IViewComponentResult> InvokeAsync()
 		{
-			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.GetAsync("https://localhost:7236/api/Abouts");
-			if (responseMessage.IsSuccessStatusCode)
-			{
-				var jsonData = await responseMessage.Content.ReadAsStringAsync();
-				var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
-				return View(values);
-			}
-			return View();
+
+            string token = "";
+            using (var HttpClient = new HttpClient())
+            {
+                var request = new HttpRequestMessage
+                {
+                    RequestUri = new Uri("https://localhost:44320/api/Logins"),  //token ?
+                    Method = HttpMethod.Post,
+                    Content = new FormUrlEncodedContent(new Dictionary<string, string>
+                    {
+                        {"client_id","MultiShopVisitorId"},
+                        {"client_secret","multishopsecret"},
+                        {"grant_type","cleint_credentials"}
+                    })
+                };
+
+                using (var response = await HttpClient.SendAsync(request))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var tokenResponse = JObject.Parse(content);
+                        token = tokenResponse["access   _token"].ToString();
+                    }
+                }
+            }
+
+
+            var client = _httpClientFactory.CreateClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var responseMessage = await client.GetAsync("https://localhost:7236/api/Abouts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+           
 		}
 	}
 }
