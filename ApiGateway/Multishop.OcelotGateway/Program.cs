8197b76f1c7ca22fee70dcc2f183c284d7d26b8b
FatihKoseoglu <koseoglufatih@gmail.com>
2025-07-03 16:14:34 +0300

    ocelot json program cs

--- ApiGateway/Multishop.OcelotGateway/Program.cs
+++ ApiGateway/Multishop.OcelotGateway/Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication().AddJwtBearer("OcelotAuthenticationScheme",opt =>
+{
+
+    opt.Authority = builder.Configuration["IdentityServerUrl"]; //kiminle beraber kullanacaðýz
+    opt.Audience = ""; //dinleyici olan 
+    opt.RequireHttpsMetadata = false;
+});
+
+
 
 IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("ocelot.json").Build();
 builder.Services.AddOcelot(configuration);
