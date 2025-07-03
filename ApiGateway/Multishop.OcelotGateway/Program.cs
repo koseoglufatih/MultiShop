47ebce8f0ff69dd52b7d1e0d18ddeb0974ae0dce
FatihKoseoglu <koseoglufatih@gmail.com>
2025-07-03 12:42:38 +0300

    program cs

--- ApiGateway/Multishop.OcelotGateway/Program.cs
+++ ApiGateway/Multishop.OcelotGateway/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("ocelo.jsont").Build();
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
