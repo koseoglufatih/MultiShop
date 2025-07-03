159bfad68fee09b6a02f5d4c04594b1089dba3b0
FatihKoseoglu <koseoglufatih@gmail.com>
2025-07-03 15:28:56 +0300

    ocelot json

--- ApiGateway/Multishop.OcelotGateway/Program.cs
+++ ApiGateway/Multishop.OcelotGateway/Program.cs
@@ -1,7 +1,14 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("ocelot.json").Build();
+builder.Services.AddOcelot(configuration);
+
 var app = builder.Build();
 
-IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("ocelo.jsont").Build();
+await app.UseOcelot();
 
 app.MapGet("/", () => "Hello World!");
 
