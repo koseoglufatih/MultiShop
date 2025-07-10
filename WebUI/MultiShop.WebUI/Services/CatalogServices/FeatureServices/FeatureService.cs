dad873ef6cfd69b15e9db19285122c335d7cd26c
FatihKoseoglu <koseoglufatih@gmail.com>
2025-07-10 14:06:27 +0300

    feature

---
+++ WebUI/MultiShop.WebUI/Services/CatalogServices/FeatureServices/FeatureService.cs
@@ -0,0 +1,53 @@
+﻿using MultiShop.DtoLayer.CatalogDtos.FeatureDtos;
+using Newtonsoft.Json;
+
+namespace MultiShop.WebUI.Services.CatalogServices.FeatureServices
+{
+    public class FeatureService : IFeatureService
+    {
+        private readonly HttpClient _httpClient;
+
+        public FeatureService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task CreateFeatureAsync(CreateFeatureDto createFeatureDto)
+        {
+            await _httpClient.PostAsJsonAsync<CreateFeatureDto>("features", createFeatureDto);
+
+        }
+
+        public async Task DeleteFeatureAsync(string id)
+        {
+            await _httpClient.DeleteAsync("features?id=" + id);
+        }
+
+        public async Task<List<ResultFeatureDto>> GetAllFeatureAsync()
+        {
+            var responseMessage = await _httpClient.GetAsync("features");
+            responseMessage.EnsureSuccessStatusCode();
+
+            var values = await responseMessage.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<ResultFeatureDto>>(values);
+
+
+        }
+
+        public async Task<GetByIdFeatureDto> GetByIdFeatureAsync(string id)
+        {
+            var responseMessage = await _httpClient.GetAsync("features/" + id);
+            var values = await responseMessage.Content.ReadFromJsonAsync<GetByIdFeatureDto>();
+            return values;
+        }
+
+        public async Task UpdateFeatureAsync(UpdateFeatureDto updateFeatureDto)
+        {
+            await _httpClient.PutAsJsonAsync<UpdateFeatureDto>("features", updateFeatureDto);
+
+
+
+        }
+
+
+    }
+}
