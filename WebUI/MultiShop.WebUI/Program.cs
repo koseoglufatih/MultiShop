288723b2a2d68583d3957c6f0019f089ecac2403
FatihKoseoglu <koseoglufatih@gmail.com>
2025-07-10 14:37:07 +0300

    brand

--- WebUI/MultiShop.WebUI/Program.cs
+++ WebUI/MultiShop.WebUI/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using MultiShop.WebUI.Handlers;
 using MultiShop.WebUI.Services;
+using MultiShop.WebUI.Services.CatalogServices.BrandServices;
 using MultiShop.WebUI.Services.CatalogServices.CategoryServices;
 using MultiShop.WebUI.Services.CatalogServices.FeatureServices;
 using MultiShop.WebUI.Services.CatalogServices.FeatureSliderServices;
@@ -99,6 +100,11 @@ builder.Services.AddHttpClient<IFeatureService, FeatureService>(opt =>
 });
 
 builder.Services.AddHttpClient<IOfferDiscountService, OfferDiscountService>(opt =>
+{
+    opt.BaseAddress = new Uri($"{values.OcelotUrl}/{values.Catalog.Path}");
+});
+
+builder.Services.AddHttpClient<IBrandService, BrandService>(opt =>
 {
     opt.BaseAddress = new Uri($"{values.OcelotUrl}/{values.Catalog.Path}");
 });
