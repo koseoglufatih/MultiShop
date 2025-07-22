4a931d0208aca9d0dd6ec9da6524c7ca02be72d5
FatihKoseoglu <koseoglufatih@gmail.com>
2025-07-22 14:38:41 +0300

    veriler ui2

--- WebUI/MultiShop.WebUI/Controllers/ShoppingCartController.cs
+++ WebUI/MultiShop.WebUI/Controllers/ShoppingCartController.cs
@@ -1,12 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.BasketDtos;
+using MultiShop.WebUI.Services.BasketServices;
+using MultiShop.WebUI.Services.CatalogServices.ProductServices;
 
 namespace MultiShop.WebUI.Controllers
 {
+    [AllowAnonymous]
     public class ShoppingCartController : Controller
     {
-        public IActionResult Index()
+
+        private readonly IProductService _productService;
+        private readonly IBasketService _basketService;
+
+        public ShoppingCartController(IProductService productService, IBasketService basketService)
         {
-            return View();
+            _productService = productService;
+            _basketService = basketService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.directory1 = "Ana Sayfa";
+            ViewBag.directory2 = "Ürünler";
+            ViewBag.directory3 = "Sepetim";
+            var basket = await _basketService.GetBasket(); // Bu eksik
+            return View(basket);
+
+        }
+
+        public async Task<IActionResult> AddBasketItem(string productId)
+        {
+            var values = await _productService.GetByIdProductAsync(productId);
+            var items = new BasketItemDto
+            {
+                ProductId = values.ProductID,
+                ProductName = values.ProductName,
+                Price = values.ProductPrice,
+                Quantity = 1
+            };
+            await _basketService.AddBasketItem(items);
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> RemoveBasketItem(string productId)
+        {
+            await _basketService.RemoveBasketItem(productId);
+            return RedirectToAction("Index");
         }
     }
 }
--- WebUI/MultiShop.WebUI/ViewComponents/ShoppingCartViewComponents/_ShoppingCartProductListComponentPartial.cs
+++ WebUI/MultiShop.WebUI/ViewComponents/ShoppingCartViewComponents/_ShoppingCartProductListComponentPartial.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Services.BasketServices;
 
 namespace MultiShop.WebUI.ViewComponents.ShoppingCartViewComponents
 {
     public class _ShoppingCartProductListComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
-        {
+        private readonly IBasketService _basketService;
 
-            return View();
+        public _ShoppingCartProductListComponentPartial(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _basketService.GetBasket();
+            return View(values);
         }
     }
 }
