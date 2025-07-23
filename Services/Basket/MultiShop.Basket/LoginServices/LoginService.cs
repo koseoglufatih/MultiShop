65e47f550f6c6071fca2ea0dc98adba2270039d9
FatihKoseoglu <koseoglufatih@gmail.com>
2025-07-23 10:25:59 +0300

    Idiscountservice

--- Services/Basket/MultiShop.Basket/LoginServices/LoginService.cs
+++ Services/Basket/MultiShop.Basket/LoginServices/LoginService.cs
@@ -9,6 +9,6 @@
             _httpContextAccessor = contextAccessor;
         }
 
-        public string GetUserId =>  _httpContextAccessor.HttpContext.User.FindFirst("sub").Value;
+        public string GetUserId => _httpContextAccessor.HttpContext.User.FindFirst("sub").Value;
     }
-    }
+}
