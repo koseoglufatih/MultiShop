a4ef2932774334dc67401ce0bd737c30033057b6
FatihKoseoglu <koseoglufatih@gmail.com>
2025-07-24 15:50:30 +0300

    orders

---
+++ WebUI/MultiShop.WebUI/Controllers/OrderController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            ViewBag.directory1 = "Multishop";
+            ViewBag.directory2 = "Siparişler";
+            ViewBag.directory3 = "Sipariş İşlemleri";
+            return View();
+        }
+    }
+}
