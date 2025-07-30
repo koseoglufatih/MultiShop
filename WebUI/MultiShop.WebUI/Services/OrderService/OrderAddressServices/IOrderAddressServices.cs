16ee17ae7344d7a7434cf2365f442645504627e0
FatihKoseoglu <koseoglufatih@gmail.com>
2025-07-30 10:27:52 +0300

    IOrderservice

---
+++ WebUI/MultiShop.WebUI/Services/OrderService/OrderAddressServices/IOrderAddressServices.cs
@@ -0,0 +1,14 @@
+﻿using MultiShop.DtoLayer.OrderDtos.OrderAddressDtos;
+
+namespace MultiShop.WebUI.Services.OrderService.OrderAddressServices
+{
+    public interface IOrderAddressServices
+    {
+        //Task<List<ResultOrderAddressDto>> GetAllOrderAddressAsync();
+        Task CreateOrderAddressAsync(CreateOrderAddressDto createOrderAddressDto);
+
+        //Task UpdateOrderAddressAsync(UpdateOrderAddressDto updateAboutDto);
+        //Task DeleteOrderAddressAsync(string id);
+        //Task<UpdateOrderAddressDto> GetByIdAboutAsync(string id);
+    }
+}
---
+++ WebUI/MultiShop.WebUI/Services/OrderService/OrderAddressServices/OrderAddressServices.cs
@@ -0,0 +1,19 @@
+﻿using MultiShop.DtoLayer.OrderDtos.OrderAddressDtos;
+
+namespace MultiShop.WebUI.Services.OrderService.OrderAddressServices
+{
+    public class OrderAddressServices : IOrderAddressServices
+    {
+        private readonly HttpClient _httpClient;
+
+        public OrderAddressServices(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+       
+        public async Task CreateOrderAddressAsync(CreateOrderAddressDto createOrderAddressDto)
+        {
+            await _httpClient.PostAsJsonAsync<CreateOrderAddressDto>("addresses", createOrderAddressDto);
+        }
+    }
+}
